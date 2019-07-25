3a9e21d43d5d9793a89ba4936320f878d244b0a4
irahozadenyse <irahodeny1@gmail.com>
2019-07-25 21:07:43 +0200

    added some jquery to project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".des").click(function() {
+        $(".cal8").toggle();
+      $(".cal9").toggle();
+   
+   
+    });
+    $(".dev").click(function() {
+        $(".cal88").toggle();
+      $(".cal99").toggle();
+    });
+
+    $(".pro").click(function() {
+        $(".cal888").toggle();
+      $(".cal999").toggle();
+    });
+  });
