9a44c7be0f3203987b3c983ef5cfda1ecc2ba5ea
irahozadenyse <irahodeny1@gmail.com>
2019-07-26 17:34:02 +0200

    added alert informing the user

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,90 @@ $(document).ready(function() {
       $(".cal999").toggle();
     });
   });
+function got(){
+  var nn=document.getElementById("nam").Value;
+  var ee=document.getElementById("em").value;
+  var cc=document.getElementById("comment").value;
+
+
+  if(nn===name){
+    alert("thank you")
+  }
+  else if(ee===email ){
+    alert("thank you")
+}
+else if(cc==="Your message here"){
+  alert("thank you")
+}
+else{
+  alert("start again")
+}
+}
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#work1").click(function(){
+    $(".wrd1").hide();
+    $("#im1").show();
+  });
+$("#work1").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work2").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work3").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work4").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work5").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work6").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work7").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+$("#work8").hover(function(){
+  $(this).css({opacity:.3});
+  $(this).fadein();
+},function(){
+  $(this).stop().css({opacity:1})
+  $(this).fadeout();
+});
+});
